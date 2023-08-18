95b43f6a91d0ae93753eea6840505ac2b36eb23e
Ion Gireada <shytiger@yahoo.com>
2023-08-18 14:43:16 +0300

    Create and test UiRenderer and UiHandlebarsR...

--- __tests__/ui-basic-fields.js
+++
@@ -1,9 +0,0 @@
-const { UiTextField } = require("../ui-basic-fields")
-
-describe('UiTextField', () => {
-
-    test('', () => {
-        let r = new UiTextField();
-        expect(r.props.usesTemplateFilename).toBeTruthy();
-    })
-})
