2b5715971f934584d17442dbcb4a7cb80abad846
Ion Gireada <shytiger@yahoo.com>
2023-08-19 14:50:38 +0300

    Created basic fields and a parent for choices

--- demo/sample-page.js
+++ demo/sample-page.js
@@ -1,8 +1,19 @@
-const { textField, colorField, numberField, booleanField, dateField, datetimeField, datetimelocalField, emailField, fileField, hiddenField, imageField, monthField, passwordField, rangeField, searchField, telField, timeField, urlField } = require("../lib/ui-field-shortcuts");
+const { textField, colorField, numberField, booleanField, dateField, datetimeField, datetimelocalField, emailField, fileField, hiddenField, imageField, monthField, passwordField, rangeField, searchField, telField, timeField, urlField, textareaField, selectField } = require("../lib/ui-field-shortcuts");
 const {saveDocument} = require("file-easy");
 const hbsr = require("hbsr");
 
+let choices = [
+    'Choice 1',
+    'Choice 2', 
+].map((label, value) => {
+    return {
+        label,
+        value: `${value}`,
+    }
+})
 let fields = [
+    selectField('Select', choices),
+    selectField('My selected choice', choices, {name: 'selected-choice'}),
     textField('First name'),
     textField('Last name*'),
     colorField('Background color'),
@@ -21,6 +32,7 @@ let fields = [
     rangeField('Range'),
     searchField('Search'),
     telField('Tel'),
+    textareaField('Textarea'),
     timeField('Time'),
     urlField('URL'),
 
@@ -36,6 +48,7 @@ fields = fields.map(field => {
 let values = {
     firstName: 'John',
     lastName: 'Doe',
+    'selected-choice': '1',
 }
 
 let result = fields.map(field => {
@@ -44,5 +57,6 @@ let result = fields.map(field => {
 
 let pageContent = hbsr.render_template('page', {content: result.join('')}, {template_path: `${__dirname}/../templates`, template_extension: '.hbs'})
 
-saveDocument('sample-page.html', pageContent)
+saveDocument('sample-page.html', pageContent) 
 
+  
