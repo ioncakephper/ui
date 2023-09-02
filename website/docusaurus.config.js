c804ceac6962a272853c5eb25198e085e140c41d
Ion Gireada <shytiger@yahoo.com>
2023-09-02 10:29:50 +0300

    Update copyright

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -142,7 +142,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} UIJS Kit Project,  Built with Docusaurus, outlined with Skelosaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
